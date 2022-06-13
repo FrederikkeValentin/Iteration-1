5e50bdc367be6a42b8843ce4191b3ed85535ce20
FrederikkeValentin <hrx539@alumni.ku.dk>
2022-06-13 10:38:15 +0200

    Initial commit

--- Test/test_Studerende.py
+++ Test/test_Studerende.py
@@ -4,8 +4,15 @@ from Model.Studerende import Studerende
 #def __init__(self, fornavn: str, efternavn: str, email:str, adresse: str, cpr_nummer: str, studienummer: str, uddannelse: str, semester_el_blok: str, login_uniplanner: str, se_skema: str, modtag_notifikation: str):
 Hent_studerende = Studerende("Emma", "Frederiksen", "emf876@alumni.ku.dk", "Amager Strandvej 42a, 2300 København S", "040797-1234", "emf876", "Sundhed og Informatik", "4. Semester", "Ja", "Ja", "JA")
 
+#TRUE
 def test_get_studienummer():
     test = Hent_studerende.get_studienummer() == "emf876"
+    assert test
+    print(test) #giver os true eller false
+
+#FALSE
+def test_get_fornavn():
+    test = Hent_studerende.get_fornavn() == "Emmi"
     assert test
     print(test) #giver os true eller false
 
